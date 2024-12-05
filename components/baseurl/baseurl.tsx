fce4aaa1c77520e573401aec9c1517d6d1eeea1f
anup354 <anupkasula5@gmail.com>
2024-12-05 15:20:11 +0545

    changes

--- components/baseurl/baseurl.tsx
+++ components/baseurl/baseurl.tsx
@@ -1,6 +1,6 @@
 
-export const BaseUrl="http://192.168.0.104:8000/api/";
+// export const BaseUrl="http://192.168.0.104:8000/api/";
 
-// export const BaseUrl="https://sajhamarket.com.np/api/";
+export const BaseUrl="https://sajhamarket.com.np/api/";
 
 
